cf1f7e3210c991c7ba218a1d7db26dd94932850b
smithag77 <agsmith@gmail.com>
2017-11-01 17:04:38 -0500

    added a class for finding LCM

---
+++ Algorithms/Numeric/LeastCommonMultiple.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.Numeric
+{
+    public static class LeastCommonMultiple
+    {
+        public static uint FindLCM(uint a, uint b)
+        {
+            return a * b / GreatestCommonDivisor.FindGCDRecursive(a, b);
+        }
+    }
+}
